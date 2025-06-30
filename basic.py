2415324a0f11481e6faf21cbb939242e1ed84017
Parul Institute of Engineering Piet Technology <piet@pu.edu>
2025-06-30 12:35:19 +0530

    .

--- basic.py
+++ basic.py
@@ -1,7 +1,14 @@
-# print("Hello World")
-# print("Hello RP")
+""" print("Hello World")
+ print("Hello RP")"""
 # name = input("What is your name:")
-# print(nAME)
-a=15
-b=10
-print(a+b)
+# print(name)
+a=input("enter A:")
+b=input("enter B:")
+c = int(a)+int(b)
+d = int(a)-int(b)
+e = int(a)*int(b)
+f = int(a)/int(b)
+g = int(a)**int(b)#for exponetnial
+h = int(a)%int(b)
+i = int(a)//int(b)#for floor division
+print(c,d,e,f,g,h,i)
