16d92827040ca8149b1b01fbdb1150e565ce8455
Parul Institute of Engineering Piet Technology <piet@pu.edu>
2025-07-01 13:38:52 +0530

    .

--- basic.py
+++ basic.py
@@ -2,7 +2,7 @@
  print("Hello RP")"""
 # name = input("What is your name:")
 # print(name)
-a=input("enter A:")
+"""a=input("enter A:")
 b=input("enter B:")
 c = int(a)+int(b)
 d = int(a)-int(b)
@@ -11,4 +11,42 @@ f = int(a)/int(b)
 g = int(a)**int(b)#for exponetnial
 h = int(a)%int(b)
 i = int(a)//int(b)#for floor division
-print(c,d,e,f,g,h,i)
+print(c,d,e,f,g,h,i)"""
+
+# logical Operators
+"""print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print(not True)
+print(not False)"""
+
+
+# li = ['RP', 'Ahemdabad', 19, 30.06, True]
+# print(li)
+
+"""a=1
+b=2
+c=3
+temp = a
+a=b
+b=temp
+temp = c
+c=b
+b=temp
+print(a,b,c)"""
+
+
+a= int(input("enter a:"))
+b= int(input("enter b:"))
+c= int(input("enter c:"))
+
+a,b,c=c,b,a
+print("after swapping a:")
+print(a,b,c)
