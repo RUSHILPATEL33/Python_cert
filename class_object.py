9f0051c72806fee4ebbbb99cabc5b99897415d51
Parul Institute of Engineering Piet Technology <piet@pu.edu>
2025-07-01 15:54:48 +0530

    .

--- class_object.py
+++ class_object.py
@@ -1,8 +1,9 @@
-class person:
+"""class person:
     def __init__(self,name,age):
         self.name=name
         self.age=age
     def greet(self):
         print(f"Hi, I'm {self.name} and I'm {self.age} years old.")
 p1=person("RP",19)
-p1.greet()
+p1.greet()"""
+
